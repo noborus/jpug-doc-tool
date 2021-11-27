e81d8b674efa49cdd828dd73d94ea6d7eff561f8
Noboru Saito <noborusai@gmail.com>
2021-11-27 12:27:48 +0900

    Added tsv output option

--- cmd/list.go
+++ cmd/list.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/jwalton/gchalk"
 	"github.com/spf13/cobra"
@@ -24,13 +25,23 @@ func list(enonly bool, jaonly bool, fileNames []string) {
 	}
 }
 
+func tsvList(fileNames []string) {
+	for _, fileName := range fileNames {
+		dicname := DICDIR + fileName + ".t"
+		catalog := loadCatalog(dicname)
+		for en, ja := range catalog {
+			fmt.Printf("%s\t%s\n", strings.ReplaceAll(en, "\n", " "), strings.ReplaceAll(ja, "\n", " "))
+		}
+	}
+}
+
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "辞書から英語と日本語訳のリストを出力する",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		var enonly, jaonly bool
+		var enonly, jaonly, tsv bool
 		var err error
 		if enonly, err = cmd.PersistentFlags().GetBool("en"); err != nil {
 			log.Println(err)
@@ -40,13 +51,25 @@ var listCmd = &cobra.Command{
 			log.Println(err)
 			return
 		}
+		if tsv, err = cmd.PersistentFlags().GetBool("tsv"); err != nil {
+			log.Println(err)
+			return
+		}
 
 		if len(args) > 0 {
+			if tsv {
+				tsvList(args)
+				return
+			}
 			list(enonly, jaonly, args)
 			return
 		}
 
 		fileNames := targetFileName()
+		if tsv {
+			tsvList(fileNames)
+			return
+		}
 		list(enonly, jaonly, fileNames)
 	},
 }
@@ -55,4 +78,5 @@ func init() {
 	rootCmd.AddCommand(listCmd)
 	listCmd.PersistentFlags().BoolP("en", "e", false, "English")
 	listCmd.PersistentFlags().BoolP("ja", "j", false, "Japanese")
+	listCmd.PersistentFlags().BoolP("tsv", "t", false, "tsv")
 }
--- go.mod
+++ go.mod
@@ -4,15 +4,10 @@ go 1.16
 
 require (
 	github.com/agnivade/levenshtein v1.1.1
-	github.com/fsnotify/fsnotify v1.5.1 // indirect
-	github.com/jwalton/gchalk v1.1.0
-	github.com/mitchellh/mapstructure v1.4.2 // indirect
-	github.com/pelletier/go-toml v1.9.4 // indirect
-	github.com/spf13/cast v1.4.1 // indirect
+	github.com/jwalton/gchalk v1.2.1
 	github.com/spf13/cobra v1.2.1
-	github.com/spf13/viper v1.8.1
-	golang.org/x/sys v0.0.0-20210915083310-ed5796bab164 // indirect
-	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	github.com/spf13/viper v1.9.0
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
 	golang.org/x/text v0.3.7
-	gopkg.in/ini.v1 v1.63.1 // indirect
+	gopkg.in/ini.v1 v1.64.0 // indirect
 )
