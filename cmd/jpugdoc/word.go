bae9977080c1edff4a7dfb316102ac1a87c2bc4f
Noboru Saito <noborusai@gmail.com>
2022-03-06 18:17:19 +0900

    Added word check

---
+++ cmd/jpugdoc/word.go
@@ -0,0 +1,31 @@
+package jpugdoc
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/jwalton/gchalk"
+)
+
+// CheckWord は英単語に対して日本語の単語が対になっているかをチェックします。
+func CheckWord(en string, ja string, fileNames []string) {
+	for _, fileName := range fileNames {
+		src, err := ReadAllFile(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pairs := Extraction(src)
+		for _, pair := range pairs {
+			if strings.Contains(pair.en, en) {
+				if !strings.Contains(pair.ja, ja) {
+					fmt.Printf("%s not include word)\n", fileName)
+					fmt.Println(gchalk.Green(pair.en))
+					fmt.Println(pair.ja)
+				}
+			}
+		}
+
+	}
+
+}
---
+++ cmd/word.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/noborus/jpug-doc-tool/cmd/jpugdoc"
+	"github.com/spf13/cobra"
+)
+
+// wordCmd represents the word command
+var wordCmd = &cobra.Command{
+	Use:   "word",
+	Short: "対応する単語が含まれているかチェックする",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		var err error
+		var en, ja string
+		if en, err = cmd.PersistentFlags().GetString("en"); err != nil {
+			log.Println(err)
+			return
+		}
+		if ja, err = cmd.PersistentFlags().GetString("ja"); err != nil {
+			log.Println(err)
+			return
+		}
+		if len(args) > 0 {
+			jpugdoc.CheckWord(en, ja, args)
+			return
+		}
+
+		fileNames := targetFileName()
+		jpugdoc.CheckWord(en, ja, fileNames)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(wordCmd)
+	wordCmd.PersistentFlags().StringP("en", "e", "", "English")
+	wordCmd.PersistentFlags().StringP("ja", "j", "", "Japanese")
+}
