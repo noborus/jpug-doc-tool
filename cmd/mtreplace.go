c46ca8a2f4daff8ceebf6261564169488d706d80
Noboru Saito <noborusai@gmail.com>
2024-02-18 11:11:51 +0900

    Added limit option

--- cmd/mtreplace.go
+++ cmd/mtreplace.go
@@ -10,6 +10,12 @@ var mtreplaceCmd = &cobra.Command{
 	Use:   "mtreplace",
 	Short: "機械翻訳のマークを実際に置き換える",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		var limit int
+		var err error
+		if limit, err = cmd.PersistentFlags().GetInt("limit"); err != nil {
+			return err
+		}
+		jpugdoc.MaxTranslate = limit
 		fileNames := expandFileNames(args)
 		return jpugdoc.MTReplace(fileNames, false)
 	},
@@ -17,4 +23,5 @@ var mtreplaceCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(mtreplaceCmd)
+	mtreplaceCmd.PersistentFlags().IntP("limit", "l", 100, "Limit for API queries")
 }
