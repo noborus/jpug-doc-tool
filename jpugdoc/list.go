b8f42906c4d8a0ede2251860c2f4817241d24f77
Noboru Saito <noborusai@gmail.com>
2024-05-18 20:30:22 +0900

    Add sprit option to list command

--- jpugdoc/list.go
+++ jpugdoc/list.go
@@ -16,12 +16,12 @@ import (
 // enOnly: 英文のみ表示する
 // jaOnly: 日本語のみ表示する
 // fileNames: ファイル名のリスト
-func List(wf bool, pre bool, enOnly bool, jaOnly bool, fileNames []string) {
+func List(wf bool, pre bool, enOnly bool, jaOnly bool, strip bool, fileNames []string) {
 	w := io.Writer(os.Stdout)
-	list(w, wf, pre, enOnly, jaOnly, fileNames)
+	list(w, wf, pre, enOnly, jaOnly, strip, fileNames)
 }
 
-func list(w io.Writer, wf bool, pre bool, enOnly bool, jaOnly bool, fileNames []string) {
+func list(w io.Writer, wf bool, pre bool, enOnly bool, jaOnly bool, strip bool, fileNames []string) {
 	for _, fileName := range fileNames {
 		if wf {
 			fmt.Fprintln(w, gchalk.Red(fileName))
@@ -30,13 +30,18 @@ func list(w io.Writer, wf bool, pre bool, enOnly bool, jaOnly bool, fileNames []
 		if err != nil {
 			log.Println(err)
 		}
-		writeCatalog(w, catalogs, pre, enOnly, jaOnly)
+		writeCatalog(w, catalogs, pre, enOnly, jaOnly, strip)
 	}
 }
 
-func writeCatalog(w io.Writer, catalogs []Catalog, suffix bool, enOnly bool, jaOnly bool) {
+func writeCatalog(w io.Writer, catalogs []Catalog, suffix bool, enOnly bool, jaOnly bool, strip bool) {
 	for _, catalog := range catalogs {
-		en := strings.Trim(catalog.en, "\n")
+		en := catalog.en
+		if strip {
+			en = stripNL(en)
+			en = strings.Join(strings.Fields(en), " ")
+		}
+		en = strings.Trim(en, "\n")
 		if !suffix && en == "" {
 			continue
 		}
@@ -47,8 +52,13 @@ func writeCatalog(w io.Writer, catalogs []Catalog, suffix bool, enOnly bool, jaO
 		if !jaOnly {
 			fmt.Fprintln(w, gchalk.Green(en))
 		}
+		ja := catalog.ja
+		if strip {
+			ja = stripNL(ja)
+			ja = strings.Join(strings.Fields(ja), " ")
+		}
 		if !enOnly {
-			fmt.Fprintln(w, strings.Trim(catalog.ja, "\n"))
+			fmt.Fprintln(w, strings.Trim(ja, "\n"))
 		}
 		if suffix {
 			fmt.Fprintln(w, gchalk.Blue(strings.Trim(catalog.post, "\n")))
@@ -58,23 +68,37 @@ func writeCatalog(w io.Writer, catalogs []Catalog, suffix bool, enOnly bool, jaO
 }
 
 // 英日辞書の内容をTSV形式で表示する
-func TSVList(fileNames []string) {
+func TSVList(strip bool, fileNames []string) {
 	w := io.Writer(os.Stdout)
-	tsvList(w, fileNames)
+	tsvList(w, strip, fileNames)
 }
 
-func tsvList(w io.Writer, fileNames []string) {
+func tsvList(w io.Writer, strip bool, fileNames []string) {
 	for _, fileName := range fileNames {
 		catalogs, err := loadCatalog(fileName)
 		if err != nil {
 			log.Println(err)
 		}
-		writeTSV(w, catalogs)
+		writeTSV(w, strip, catalogs)
 	}
 }
 
-func writeTSV(w io.Writer, catalogs []Catalog) {
+func writeTSV(w io.Writer, strip bool, catalogs []Catalog) {
 	for _, catalog := range catalogs {
-		fmt.Fprintf(w, "%s\t%s\n", strings.ReplaceAll(catalog.en, "\n", " "), strings.ReplaceAll(catalog.ja, "\n", " "))
+		en := catalog.en
+		if strip {
+			en = stripNL(en)
+			en = strings.Join(strings.Fields(en), " ")
+		} else {
+			en = strings.ReplaceAll(en, "\n", " ")
+		}
+		ja := catalog.ja
+		if strip {
+			ja = stripNL(ja)
+			ja = strings.Join(strings.Fields(ja), " ")
+		} else {
+			ja = strings.ReplaceAll(ja, "\n", " ")
+		}
+		fmt.Fprintf(w, "%s\t%s\n", en, ja)
 	}
 }
